36379558983bc325e88cddcafc56bbd61fb5fe20
Simbitek <wambuiweruh@gmail.com>
2026-09-23 15:39:22 +0300

    creating the about section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+
 
 
 export const Home = () => {
@@ -21,6 +23,7 @@ export const Home = () => {
             {/* Main content */}
             <main>
                 <HeroSection />
+                <AboutSection />
             </main>
 
             {/* Footer */}
